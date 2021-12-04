7d6a35be0b164a452f4f6526d4e6e7087b0c5f79
shruti0603 <66424592+shruti0603@users.noreply.github.com>
2021-12-04 12:37:13 +0530

    Update thankyou.php

--- Payment-Gateway-Integration-master/Payment-Gateway-Integration-master/src/thankyou.php
+++ Payment-Gateway-Integration-master/Payment-Gateway-Integration-master/src/thankyou.php
@@ -18,7 +18,7 @@ display:inline-block;
 <?php
 include 'instamojo/Instamojo.php';
 // write your own API key and Auth Token
-$api = new Instamojo\Instamojo('test_3146bcda92c0d3f7817139a0475','test_dd4b7eda094e89351522e254bb3','https://test.instamojo.com/api/1.1/');
+$api = new Instamojo\Instamojo('API Key','Auth Token','https://test.instamojo.com/api/1.1/');
 $payid = $_GET["payment_request_id"];
 try{
 	$response = $api->paymentRequestStatus($payid);
