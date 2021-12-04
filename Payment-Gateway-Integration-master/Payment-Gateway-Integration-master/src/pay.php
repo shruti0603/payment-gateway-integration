bfed1ad4e26888cca0e26dd75aea9c47d2bb14c5
shruti0603 <66424592+shruti0603@users.noreply.github.com>
2021-12-04 12:36:25 +0530

    Update pay.php

--- Payment-Gateway-Integration-master/Payment-Gateway-Integration-master/src/pay.php
+++ Payment-Gateway-Integration-master/Payment-Gateway-Integration-master/src/pay.php
@@ -7,7 +7,7 @@ $email = $_POST["email"];
 
 include 'instamojo/Instamojo.php';
 // write your own API Key and Auth Token
-$api = new Instamojo\Instamojo('test_3146bcda92c0d3f7817139a0475','test_dd4b7eda094e89351522e254bb3', 'https://test.instamojo.com/api/1.1/');
+$api = new Instamojo\Instamojo('API Key','Auth Token', 'https://test.instamojo.com/api/1.1/');
 
 try{
 	$response = $api->paymentRequestCreate(array(
@@ -21,7 +21,7 @@ try{
 		"allow_repeated_payments" => false,
 		// add your hosted website url with the redirected page. here the redirected page is thankyou.php
 		// e.g. http://pgi.epizy.com/thankyou.php
-		"redirect_url" => "https://sparks-pgi.000webhostapp.com/thankyou.php",
+		"redirect_url" => "href",
 		
 
 	));
